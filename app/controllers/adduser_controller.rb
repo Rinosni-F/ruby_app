f84166b1a61abf2b56f5fc81fbb10f4f396f5592
Rinosni-F <arul.rinoshini@asirtech.com>
2024-05-23 16:07:53 +0530

    adduser

--- app/controllers/adduser_controller.rb
+++ app/controllers/adduser_controller.rb
@@ -1,50 +1,67 @@
-class Adduser 
+require 'bcrypt'
 
-        def index(env)
-            headers = {'Content-Type' => 'text/html'}
-            response = File.read('app/views/home/adduser.html')
-            [200, headers, [response]]
-        end
-        def call(env)
-            request = Rack::Request.new(env)
-        
-            if request.post? && request.path == '/add_user'
-              handle_form_submission(request)
-            else
-              render_add_user_form
-            end
-          end
-        
-          private
-        
-          def handle_form_submission(request)
-            # Extract form data
-            username = request.params['username']
-            email = request.params['email']
-            password = request.params['password']
-        
-            # Insert user data into the database
-            insert_user(username, email, password)
-        
-            # Redirect to a success page or display a success message
-            [302, {'Location' => '/success'}, []]
-          end
-        
-          def insert_user(username, email, password)
-            # Establish a connection to the database
-            # For simplicity, assuming you already have a database connection setup
-        
-            # Prepare and execute SQL query to insert user data
-            query = "INSERT INTO user (username, email, password) VALUES (?, ?, ?)"
-            statement = @client.prepare(query)
-            statement.execute(username, email, password)
-          end
-        
-          def render_add_user_form
-            headers = {'Content-Type' => 'text/html'}
-            response = File.read('app/views/home/adduser.html')
-            [200, headers, [response]]
-          end
+class AddUserController
+  def initialize(client)
+    @client = client
+  end
+
+  def call(env)
+    request = Rack::Request.new(env)
+
+    if request.post? && request.path == '/add_user'
+      handle_form_submission(request)
+    else
+      render_add_user_form
+    end
+  end
+
+  private
+
+  def handle_form_submission(request)
+    username = request.params['username']
+    email = request.params['email']
+    password = BCrypt::Password.create(request.params['password'])
+
+    if insert_user(username, email, password)
+      render_success_message
+    else
+      render_failure_message
+    end
+  end
+
+  def insert_user(username, email, password)
+    query = "INSERT INTO user (username, email, password) VALUES (?, ?, ?)"
+    statement = @client.prepare(query)
+    statement.execute(username, email, password)
+    true # Return true to indicate success
+  rescue StandardError => e
+    puts "Error inserting user: #{e.message}" # Log the error message
+    false # Return false to indicate failure
+  end
+
+  def render_add_user_form
+    headers = {'Content-Type' => 'text/html'}
+    response = File.read('app/views/home/adduser.html')
+    [200, headers, [response]]
+  end
+
+  def render_success_message
+    headers = {'Content-Type' => 'text/html'}
+    response = modify_html('success-message')
+    [200, headers, [response]]
+  end
+
+  def render_failure_message
+    headers = {'Content-Type' => 'text/html'}
+    response = modify_html('failure-message')
+    [200, headers, [response]]
+  end
+
+  def modify_html(section_id)
+    html = File.read('app/views/home/adduser.html')
+    html.gsub!(%r{id="login-form" class="center"}, 'id="login-form" class="center hidden"')
+    html.gsub!(%r{id="success-message" class="center hidden"}, 'id="success-message" class="center"') if section_id == 'success-message'
+    html.gsub!(%r{id="failure-message" class="center hidden"}, 'id="failure-message" class="center"') if section_id == 'failure-message'
+    html
+  end
 end
-
-  
--- config/routes.rb
+++ config/routes.rb
@@ -1,28 +1,26 @@
 class Routes
-    def initialize(client)
-      @client = client
-    end
-  
-    def call(env)
-      case env['PATH_INFO']
-      when '/'
-        LoginController.new(@client).call(env)
-      when '/submit'
-        LoginController.new(@client).call(env)
-      when '/app/views/home/adduser.html'
-        Adduser.new.index(env)
-      when '/add_user'
-        Adduser.new(@client).call(env)
-        # Add more routes here
-      else
-        not_found
-      end
-    end
-  
-    private
-  
-    def not_found
-      [404, {'Content-Type' => 'text/html'}, ['Page Not Found']]
+  def initialize(client)
+    @client = client
+  end
+
+  def call(env)
+    case env['PATH_INFO']
+    when '/'
+      LoginController.new(@client).call(env)
+    when '/submit'
+      LoginController.new(@client).call(env)
+    when '/adduser'
+      AddUserController.new(@client).call(env)
+    when '/add_user'
+      AddUserController.new(@client).call(env)
+    else
+      not_found
     end
   end
-  
+
+  private
+
+  def not_found
+    [404, {'Content-Type' => 'text/html'}, ['Page Not Found']]
+  end
+end
